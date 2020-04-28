0d2413c51ff3681f8fff1add366cbf78a6ba803f
farhana <farhanamitu70@gmail.com>
2020-04-28 02:41:33 -0400

    Working on responsive adjusments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,11 +6,13 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
-        
+
     </header>
 
 );
